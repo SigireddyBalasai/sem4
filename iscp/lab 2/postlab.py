2d80d17184ed775cc46bb1574530619dae504e79
Your Name <you@example.com>
2023-08-24 14:17:26 +0530

    upgraded postlab

--- iscp/lab 2/postlab.py
+++ iscp/lab 2/postlab.py
@@ -1,16 +1,24 @@
 class StockSpanner:
     def __init__(self):
         self.arr = []
-        in
-    def next(n):
+        self.solu = []
+    def next(self,n):
         count = 1
         self.arr.append(n)
         for i in range(len(self.arr)):
-            if(self.arr[i] <= n):
+            if(self.arr[i] < n):
                 count += 1
-        return cost
+        print(count)
+        self.solu.append(count)
+        return count
 
-map_command = input().split()
-input_command = input().split()
+    def __str__(self):
+        return ",".join(list(map(str,self.solu)))
+
+input_command = list(map(int,input().split()))
+ok = StockSpanner()
+for i in input_command:
+    ok.next(i)
+print(ok)
 
         
